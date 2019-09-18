edacf16ad577f1d07347bf095c6e8d7f2cb61886
ngodoannghia <20162905@student.hust.edu.vn>
2019-09-18 23:01:07 +0700

    update

---
+++ Web/js/index.js
@@ -0,0 +1,60 @@
+(function(){
+
+    var app = angular.module('app',[]);
+
+    
+    
+    
+    
+
+    app.controller('mycontroller',['$scope', '$interval', function($scope,$interval){
+        var i = 0;
+        $scope.bgs=['./img/bgbook1.png', './img/bg1.png'];
+        $scope.books = ['./img/imgbook1.png', './img/imgbook2.png'];
+        $scope.texts = ['./img/textbook1.png', './img/textbook2.png'];
+        $scope.titles = ['./img/titlebook1.png', './img/titlebook2.png'];
+
+        $scope.bg = $scope.bgs[i % 2];
+        $scope.book = $scope.books[i % 2];
+        $scope.text = $scope.texts[i % 2];
+        $scope.title = $scope.titles[i%2];
+        i++;
+
+        slideshow = function(){
+           $scope.bg = $scope.bgs[i % 2];
+           $scope.book = $scope.books[i % 2];
+           $scope.text = $scope.texts[i % 2];
+           $scope.title = $scope.titles[i%2];
+
+           i++;
+        }
+
+        $scope.leftslide = function(){
+            i = (i+2-1)%2;
+         
+            $scope.bg = $scope.bgs[i % 2];
+            $scope.book = $scope.books[i % 2];
+            $scope.text = $scope.texts[i % 2];
+            $scope.title = $scope.titles[i%2];          
+        }
+
+        $scope.rightslide = function(){
+            i++;
+           
+            $scope.bg = $scope.bgs[i % 2];
+            $scope.book = $scope.books[i % 2];
+            $scope.text = $scope.texts[i % 2];
+            $scope.title = $scope.titles[i%2];
+            
+        }
+
+
+        $interval(function(){slideshow();}, 5000);
+    }]);
+
+
+
+
+
+
+})();
